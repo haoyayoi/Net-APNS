24999ff6a638565a612914f662ea86395fa7b9a6
haoyayoi <st.hao.yayoi@gmail.com>
2009-08-02 12:48:15 +0900

    initial commit

---
+++ lib/Net/APNS.pm
@@ -0,0 +1,33 @@
+package Net::APNS;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::APNS -
+
+=head1 SYNOPSIS
+
+  use Net::APNS;
+
+=head1 DESCRIPTION
+
+Net::APNS is
+
+=head1 AUTHOR
+
+haoyayoi E<lt>st.hao.yayoi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
