c1b5936c5b6f61f9f9c63c57f9b9cb41c8519ef1
haoyayoi <st.hao.yayoi@gmail.com>
2009-10-18 14:50:33 +0900

    update pod

--- lib/Net/APNS.pm
+++ lib/Net/APNS.pm
@@ -1,21 +1,19 @@
 package Net::APNS;
 
-use Moose;
+use Any::Moose;
 use Net::APNS::Notification;
-our $VERSION = '0.0101';
+our $VERSION = '0.02';
 
 sub notify {
-    my ($self, $args) = @_;
+    my ( $self, $args ) = @_;
     return Net::APNS::Notification->new(
-        cert        => $args->{cert},
-        key         => $args->{key},
-        passwd      => $args->{passwd},
+        cert   => $args->{cert},
+        key    => $args->{key},
+        passwd => $args->{passwd},
     );
 }
 
 __PACKAGE__->meta->make_immutable;
-no Moose;
-
 1;
 __END__
 
@@ -36,16 +34,83 @@ Net::APNS - Apple Push Notification Service for perl.
   $Notifier->message("message");
   $Notifier->badge(4);
   $Notifier->write;
+
 =head1 DESCRIPTION
 
 Net::APNS is Apple Push Notification Service.
 Push message to iPhone and get unavalble-devicetoken.
+
+=head1 METHOD
+
+=over 2
+
+=item notify()
+
+Return push client. Need specify parameters.
+
+=back
+
+=head2 PARAMETERS
+
+=over 4
+
+=item Cert
+
+Server certification file. 
+
+=item Key
+
+Server certification key file.
+
+=item passwd
+
+certification password. (option)
+
+=back
+
+=head2 PUSH
+
+=over 3
+
+=item At all in one time.
+
+Payload contains message, badge and more.
+
+  $APNS->devicetoken($devicetoken);
+  $APNS->payload($payload);
+  $APNS->write;
+
+or
+
+  $APNS->devicetoken($devicetoken);
+  $APNS->write($payload);
+
+=item specify message and badge
+
+This style can push specify message and badge only.
+
+  $APNS->devicetoken($devicetoken);
+  $APNS->message($message);
+  $APNS->badge($badge);
+  $APNS->write;>
+
+or
+
+  $APNS->devicetoken($devicetoken);
+  $APNS->write({
+      message => $message,
+      badge   => $badge,
+  });
+
+=back
 
 =head1 AUTHOR
 
 haoyayoi E<lt>st.hao.yayoi@gmail.comE<gt>
 
 =head1 SEE ALSO
+
+L<Net::APNS::Notification>
 
 =head1 LICENSE
 
